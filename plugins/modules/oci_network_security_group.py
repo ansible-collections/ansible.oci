4d004e6716f65b3d28f43fe09158dd1ff67dcdbb
Ron Gershburg <rgershbu@redhat.com>
2026-09-01 11:42:42 +0300

    fix(oci_network_security_group): use UpdateFieldSpec objects

--- plugins/modules/oci_network_security_group.py
+++ plugins/modules/oci_network_security_group.py
@@ -146,6 +146,7 @@ from ansible_collections.ansible.oci.plugins.module_utils.oci_common import (
 )
 from ansible_collections.ansible.oci.plugins.module_utils.oci_resource import (
     OciResourceBase,
+    UpdateFieldSpec,
 )
 
 imported_oci_sdk = import_oci_sdk()
@@ -184,24 +185,21 @@ class OciNetworkSecurityGroupModule(OciResourceBase):
     update_method_name = "update_network_security_group"
     update_details_name = "update_network_security_group_details"
     update_wait_states = WAIT_FOR_NETWORK_SECURITY_GROUP_STATES
-    update_field_specs = [
-        {
-            "param_name": "name",
-            "resource_field": "display_name",
-            "update_field": "display_name",
-            "is_mutable": True,
-        },
-        {
-            "param_name": "vcn_id",
-            "resource_field": "vcn_id",
-            "is_mutable": False,
-        },
-        {
-            "param_name": "compartment_id",
-            "resource_field": "compartment_id",
-            "is_mutable": False,
-        },
-    ]
+    update_field_specs = (
+        UpdateFieldSpec(
+            param_name="name",
+            resource_field="display_name",
+            is_mutable=True,
+        ),
+        UpdateFieldSpec(
+            param_name="vcn_id",
+            is_mutable=False,
+        ),
+        UpdateFieldSpec(
+            param_name="compartment_id",
+            is_mutable=False,
+        ),
+    )
 
     def get_resource_response(self, resource_id):
         return self.call_with_retry(
