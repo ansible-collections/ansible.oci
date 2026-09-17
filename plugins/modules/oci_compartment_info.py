171c91bddc3a51e0e179b3e35713645731b41918
Ron Gershburg <rgershbu@redhat.com>
2026-09-17 16:26:22 +0300

    Fix Compartment Module

--- plugins/modules/oci_compartment_info.py
+++ plugins/modules/oci_compartment_info.py
@@ -117,7 +117,9 @@ from ansible_collections.ansible.oci.plugins.module_utils.oci_info import (
     OciInfoBase,
 )
 
-oci, HAS_OCI_SDK = import_oci_sdk()
+imported_oci_sdk = import_oci_sdk()
+oci = imported_oci_sdk[0]
+HAS_OCI_SDK = imported_oci_sdk[1]
 
 
 class OciCompartmentInfoModule(OciInfoBase):
